32aa5f15ca7dcfede85dcf25a7a5839575f926b1
ruhomor <ruhomor@icloud.com>
2020-11-03 14:43:56 +0300

    a

--- gaem/Application.cpp
+++ gaem/Application.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
-#include <GLFW/glfw3.h>
+#include "gaem.h"
+
+Map::Map(int m, int n) //Matrix[n][m]
+{
+	this->resize(n, vector<int>(m));
+	init = 1;
+}
+
+Map::Map()
+{
+	init = 0;
+}
+
+void	Map::display()
+{
+	for (int i = 0; i < (*this).size(); i++)
+	{
+		for (int j = 0; j < (*this)[i].size(); j++)
+			 std::cout << (*this)[i][j] << ' ';
+		std::cout << '\n';
+	}
+}
+
+bool	Map::isInit()
+{
+	return (init);
+}
+
+void	Map::setInit()
+{
+	init = true;
+}
 
 static void		key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -7,16 +37,54 @@ static void		key_callback(GLFWwindow* window, int key, int scancode, int action,
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-int		main(void)
+static void		printUsage()
 {
-	GLFWwindow* window;
+	printf("Usage: ./Application OPTIONAL: {map size x} {map size y}\n");
+}
+
+static void		printWrong()
+{
+	printf("Wrong Number of Arguments\n"); //TODO print to errorstream?
+}
+
+int		main(int argc, char **argv) //TODO map size
+{
+	GLFWwindow*		window;
+	Map				gameMap;
+
+	/* reading map size from command line */
+	if (argc != 1)
+	{
+		if (argc == 3)
+		{
+			int		m = atoi(argv[1]), n = atoi(argv[2]);
+			gameMap.resize(n, std::vector<int>(m));
+			gameMap.setInit();
+		}
+		else
+		{
+			printWrong();
+			printUsage();
+			return (-2);
+		}
+	}
+
+	if (!(gameMap.isInit()))
+		gameMap.resize(MAPY, std::vector<int>(MAPX));
+
+	/* initializing gameMap part2 */
+
+	//TODO try compiling algorithms from the outside
+	//pretend this part is finished and do everything else
+
+	gameMap.display(); //debug
 
 	/* Initialize the library */
 	if (!glfwInit())
 		return (-1);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "GAEM", NULL, NULL);
+	window = glfwCreateWindow(WINX, WINY, "GAEM", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
--- gaem/gaem.h
+++ gaem/gaem.h
@@ -6,13 +6,33 @@
 /*   By: Ruslan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 12:19:49 by Ruslan            #+#    #+#             */
-/*   Updated: 2020/11/03 12:22:14 by Ruslan           ###   ########.fr       */
+/*   Updated: 2020/11/03 14:41:05 by Ruslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAEM_H
 
-# include <iostream>
 # include <GLFW/glfw3.h>
+# include <vector>
+# include <iostream>
+# define WINX 640
+# define WINY 480
+# define MAPX 4
+# define MAPY 3
+
+class	Map : public std::vector < std::vector<int> >
+{
+private:
+//	std::vector < std::vector < int > > matrix;
+	bool	init = false;
+
+public:
+	Map();
+	Map(int m, int n);
+	bool isInit();
+	void setInit();
+	void gameStep();
+	void display();
+};
 
 #endif

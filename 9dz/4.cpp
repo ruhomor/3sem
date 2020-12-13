7e6ff8a920dd6bcf5bddb50e14486c25070f08a1
ruhomor <ruhomor@icloud.com>
2020-12-13 20:15:43 +0300

    a

--- 9dz/4.cpp
+++ 9dz/4.cpp
@@ -6,23 +6,22 @@
 /*   By: Ruslan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 20:08:24 by Ruslan            #+#    #+#             */
-/*   Updated: 2020/12/13 20:13:16 by Ruslan           ###   ########.fr       */
+/*   Updated: 2020/12/13 20:15:19 by Ruslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-//с выражением свертки
+void	printWords(void)
+{
+	return ;
+}
+
 template <typename First, typename... Types>
 void	printWords(First mem, Types... args)
 {
 	std::cout << mem;
 	printWords(args...);
-}
-
-void	printWords(void)
-{
-	return ;
 }
 
 int		main()
